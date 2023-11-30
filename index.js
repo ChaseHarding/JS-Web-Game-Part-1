153608b64a07d6d90e843486464977b23afe0a35
ChaseHarding <chaseharding965@gmail.com>
2023-11-29 16:54:52 -0800

    added pine tree

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+//This is for our Character//
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+//This is for our Pine Tree//
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
